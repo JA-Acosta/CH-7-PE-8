ed41bc0f0f9347da06fd3335a3a02a6751d4faaf
JAAR <joe.aaron.acosta@outlook.com>
2023-05-22 10:21:47 -0400

    Version 1.1: Include Type Structure

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 // JAAR
-// 05/19/2023
+// 05/22/2023
 // C++ Primer Plus 6th Ed
 // CH 7 Program Ex 8
-// Version 1
+// Version 1.1
 
 // Updates:
+// replace expense array with structure.
+// +total_expense in structure
+// +average_expenditure double in structure.
 
 
 // Future goals:
-// create a function that will identify the season with the least, greatest, and
-// average expenditure.
+// create a function that will identify the season with the least and greatest
+// expense
+	// !can add this to the structure.
 
 
 // Potential Use cases/errors:
@@ -29,45 +33,51 @@ using namespace std;
 // Global Constants and Structures:
 const int NUMBER_OF_SEASONS = 4;
 const char* SEASON_NAMES[4] = {"Spring", "Summer", "Fall", "Winter"};
+struct expenses {
+	double seasonal_Expense[NUMBER_OF_SEASONS]{};
+	double total_Expenses = 0;
+	double average_Expense = 0;
+};
 
 // Prototypes:
-void fill_Expense_List(double*);
-void show_Seasonal_Expense(double[]);
+void fill_Expense_List(expenses* annual);
+void show_Seasonal_Expense(const expenses* annual);
 int main()
 {
+	expenses annual;
 	cout << "Please input the seasonal expense for this year." << endl;
-	double seasonal_Expenses[NUMBER_OF_SEASONS];
-	fill_Expense_List(seasonal_Expenses);
-	show_Seasonal_Expense(seasonal_Expenses);
+	fill_Expense_List(&annual);
+	show_Seasonal_Expense(&annual);
 }
 
 
 // TODO: Update function so it verifies that a double was inputted.
-// Prompts the user to input the expenditure for each season.
+// Prompts the user to input the expenditure for each season and uptate the
+// total annual expenditure.
 // !assume the user will input a double each time;
 // Param: double* expense
-void fill_Expense_List(double* expense)
+void fill_Expense_List(expenses* annual)
 {
 	for (int i = 0; i < NUMBER_OF_SEASONS; i++)
 	{
 		cout << "\tEnter " << SEASON_NAMES[i] << " expenses: ";
-		cin >> expense[i];
+		cin >> annual->seasonal_Expense[i];
+		annual->total_Expenses += annual->seasonal_Expense[i];
 	}
+	annual->average_Expense = annual->total_Expenses / NUMBER_OF_SEASONS;
 }
 
 
 // Takes a list of expenses and prints them out to the console for the user.
 // Param: double expense[]
-void show_Seasonal_Expense(double expense[])
+void show_Seasonal_Expense(const expenses* annual)
 {
 	cout << "EXPENSES\n";
-	double total_Expense = 0;
 	for (int i = 0; i < NUMBER_OF_SEASONS; i++)
 	{
-		cout << SEASON_NAMES[i] << ": $" << expense[i]
-		<< endl;
-		total_Expense += expense[i];
+		cout << SEASON_NAMES[i] << ": $" << annual->seasonal_Expense[i]
+		<< "\n";
 	}
-	cout << "Total: $" << total_Expense;
-
+	cout << "Total: $" << annual->total_Expenses << "\n";
+	cout << "Average: $" << annual->average_Expense << "\n";
 }
